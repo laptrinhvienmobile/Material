20d192651265de3eca47810217e72d6ee37cec92
Vũ Quốc Toàn <khongbaogiobocuocso1@gmail.com>
2025-06-05 22:27:58 +0700

    Thêm Widget GestureDetector

---
+++ lib/widgets/gesture_detector_example.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class GestureDetectorExample extends StatelessWidget {
+  const GestureDetectorExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
+          child: GestureDetector(
+            onTap: () => debugPrint('GestureDetector'),
+            child: Container(
+              width: double.infinity,
+              height: 50.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.pink.shade200,
+              ),
+              child: const Icon(
+                Icons.add,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
