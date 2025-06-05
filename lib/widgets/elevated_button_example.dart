dab6a394c186e64253f24c47670444d2b6b2aaf8
Vũ Quốc Toàn <khongbaogiobocuocso1@gmail.com>
2025-06-05 14:45:11 +0700

    Thêm Widget ElevatedButton

---
+++ lib/widgets/elevated_button_example.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ElevatedButtonExample extends StatelessWidget {
+  const ElevatedButtonExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
+          child: SizedBox(
+            width: double.infinity,
+            height: 50.0,
+            child: ElevatedButton(
+              style: ButtonStyle(
+                backgroundColor: WidgetStatePropertyAll(Colors.pink),
+                foregroundColor: WidgetStatePropertyAll(Colors.white),
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+              ),
+              onPressed: () => debugPrint('ElevatedButton'),
+              child: Text(
+                'Bấm vào đây',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
