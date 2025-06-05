d24243e70a48158ce58bf0ab3278b71dbf13c004
Vũ Quốc Toàn <khongbaogiobocuocso1@gmail.com>
2025-06-05 14:55:33 +0700

    Thêm Widget FilledButton

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/widgets/outlined_button_example.dart';
+import 'package:project/widgets/filled_button_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: OutlinedButtonExample(),
+      home: FilledButtonExample(),
     );
   }
 }
---
+++ lib/widgets/filled_button_example.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class FilledButtonExample extends StatelessWidget {
+  const FilledButtonExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 10.0,
+          ),
+          child: SizedBox(
+            width: double.infinity,
+            height: 50.0,
+            child: FilledButton(
+              style: ButtonStyle(
+                foregroundColor: WidgetStatePropertyAll(Colors.white),
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                  ),
+                ),
+              ),
+              onPressed: () => debugPrint('FilledButton'),
+              child: Text(
+                'Bấm vào đây',
+                style: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
