2d75542cdc45b3bdbbffcb56d973dc6eea974258
Vũ Quốc Toàn <khongbaogiobocuocso1@gmail.com>
2025-06-05 21:30:29 +0700

    Thêm Widget TextField

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/widgets/clip_r_rect_example.dart';
+import 'package:project/widgets/text_field_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: ClipRRectExample(),
+      home: TextFieldExample(),
     );
   }
 }
