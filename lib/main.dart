fa7bb8a0ccb178cced3b6b898fcde299830218b3
Vũ Quốc Toàn <khongbaogiobocuocso1@gmail.com>
2025-06-05 21:13:45 +0700

    Thêm Widget ClipRRect

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/widgets/bottom_app_bar_example.dart';
+import 'package:project/widgets/clip_r_rect_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BottomAppBarExample(),
+      home: ClipRRectExample(),
     );
   }
 }
---
+++ lib/widgets/clip_r_rect_example.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ClipRRectExample extends StatelessWidget {
+  const ClipRRectExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ClipRRect(
+          clipBehavior: Clip.hardEdge,
+          borderRadius: BorderRadius.circular(20),
+          child: Image.network(
+            "https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_1280.jpg",
+            width: 120.0,
+            height: 120.0,
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
