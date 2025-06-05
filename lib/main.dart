853e29521d97ddbd233ee90cec985fd35263745d
Vũ Quốc Toàn <khongbaogiobocuocso1@gmail.com>
2025-06-05 12:23:43 +0700

    Thêm Widget BottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/widgets/tab_bar_example.dart';
+import 'package:project/widgets/bottom_navigation_bar_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: TabBarExample(),
+      home: BottomNavigationBarExample(),
     );
   }
 }
