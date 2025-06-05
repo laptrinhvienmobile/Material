b55788fc64161899a048b24d22c2a9abc269277c
Vũ Quốc Toàn <khongbaogiobocuocso1@gmail.com>
2025-06-05 12:41:37 +0700

    Thêm Widget ColoredBox

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:project/widgets/stepper_example.dart';
+import 'package:project/widgets/colored_box_example.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: StepperExample(),
+      home: ColoredBoxExample(),
     );
   }
 }
---
+++ lib/widgets/colored_box_example.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class ColoredBoxExample extends StatelessWidget {
+  const ColoredBoxExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ColoredBox(
+          color: Colors.green,
+          child: SizedBox(
+            width: 300.0,
+            height: 300.0,
+          ),
+        ),
+      ),
+    );
+  }
+}
